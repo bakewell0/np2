b3ec4e61c27dedf48b905e0302a9902a368f9596
郑炬波 <jubo.zheng@zhangmen.com>
2020-11-26 17:51:12 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div >
+       hello ,world 
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: [
+        // 'webpack/hot/only-dev-server',
+        // './js/app.js'
+        'src/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    // module: {
+    //     loaders: [
+    //     { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude:     /node_modules/ },
+    //     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+    //     { test: /\.css$/, loader: "style!css" },
+    //     {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
+    //     ]
+    // },
+    resolve:{
+        extensions:['','.js','.json']
+    },
+    // plugins: [
+    //     new webpack.NoErrorsPlugin()
+    // ]
+};
