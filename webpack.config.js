7d3e08ace71a83b039458e0fed767d28f6d4b22e
郑炬波 <jubo.zheng@zhangmen.com>
2020-11-27 11:40:28 +0800

    t

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     entry: [
-        // 'webpack/hot/only-dev-server',
-        // './js/app.js'
-        'src/index.js'
+        './src/index.js'
     ],
     output: {
-        path: __dirname + '/dist',
+        path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
     },
+    module:{   // 第三方模块配置规则
+        rules:[
+            {test:/\.js|jsx$/ , use:{
+                loader:'babel-loader',
+                options: {
+                    presets:['@babel/preset-env']}
+            }, exclude: /node_modules/}    // 添加排除项
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.join(__dirname, './src/index.html'),
+            filename: 'index.html' // 生成的内存中首页的名称
+        })
+    ]
     // module: {
     //     loaders: [
     //     { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude:     /node_modules/ },
@@ -17,9 +33,9 @@ module.exports = {
     //     {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
     //     ]
     // },
-    resolve:{
-        extensions:['','.js','.json']
-    },
+    // resolve:{
+    //     extensions:['','.js','.json']
+    // },
     // plugins: [
     //     new webpack.NoErrorsPlugin()
     // ]
