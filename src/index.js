6321e0c2698d06615de1551838e4dfe4db0aaee9
z-teacher <1536164129@qq.com>
2020-11-29 21:09:02 +0800

    loader

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './style/index.less';
 import './style/index.stylus';
+// import goo from './dl1.png';
+import logoSvg from './icons/google_logo.svg'
 
 function App () {
   return (
     <div className = "box selected" >
        hello ,world
+       <img src={logoSvg}></img>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,74 +3,95 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
-    module:{   // 第三方模块配置规则
-        rules:[
-            {
-                test:/\.js|jsx$/ , 
-                use:{
-                    loader:'babel-loader',
-                    options: {
-                        presets:['@babel/preset-env']}
-                    }, 
-                exclude: /node_modules/
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.stylus$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'stylus-loader'
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/,
-                enforce: 'pre' // 预处理
-            }
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: { // 第三方模块配置规则
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: path.join(__dirname, './src/index.html'),
-            filename: 'index.html' // 生成的内存中首页的名称
-        })
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.stylus$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre' // 预处理
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            // 当加载的图片小于limit时，会直接用url-loader将图片的格式编译成base64格式的
+            // 当加载图片大于limit时，需要使用file-loader来编译，并且打包到dist文件夹下面
+            options: {
+              limit: 16000,
+              name: 'img/[name].[hash:8].[ext]' // 对打包后的图片命名进行相关的处理，表示在dist文件夹下建一个img文件夹保存图片，同时图片的名字是原来的名字加上8位hash值，再加上图片的扩展名
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader',
+        options: {
+          limit: 16000,
+          name: 'img/[name].[hash:8].[ext]' // 对打包后的图片命名进行相关的处理，表示在dist文件夹下建一个img文件夹保存图片，同时图片的名字是原来的名字加上8位hash值，再加上图片的扩展名
+        }
+      }
     ]
-    // module: {
-    //     loaders: [
-    //     { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude:     /node_modules/ },
-    //     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-    //     { test: /\.css$/, loader: "style!css" },
-    //     {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
-    //     ]
-    // },
-    // resolve:{
-    //     extensions:['','.js','.json']
-    // },
-    // plugins: [
-    //     new webpack.NoErrorsPlugin()
-    // ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      filename: 'index.html' // 生成的内存中首页的名称
+    })
+  ]
+  // module: {
+  //     loaders: [
+  //     { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude:     /node_modules/ },
+  //     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+  //     { test: /\.css$/, loader: "style!css" },
+  //     {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
+  //     ]
+  // },
+  // resolve:{
+  //     extensions:['','.js','.json']
+  // },
+  // plugins: [
+  //     new webpack.NoErrorsPlugin()
+  // ]
 };
